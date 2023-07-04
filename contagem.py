7d147cdbe754e1ed363067fbb4f62979127f119d
paulohen98 <70858179+paulohen98@users.noreply.github.com>
2023-07-04 13:32:28 -0300

    Contagem regressiva com entrada de dados

---
+++ contagem.py
@@ -0,0 +1,14 @@
+import time
+
+print("Bem-vindo à Contagem regressiva.")
+num1 = int(input("Digite um número: "))
+
+while num1 >= 0:
+    print(num1)
+    time.sleep(0.5)
+    num1 -= 1
+
+print("Fim da Contagem.")
+
+   
+    
